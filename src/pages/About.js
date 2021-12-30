8b07f069f4ac9a64dda04cef17030968018fbcd5
MartinSimeonovvv <marti_191997@abv.bg>
2021-12-30 13:08:14 +0200

    Bulding HTML/CSS, working Navigation and partly searching functionality

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Simple yet stand-out, Restaurant Ours brings the outside in, to create
+        an unexpected space hidden in the heart of Knightsbridge. One of the
+        finest restaurants in South Kensington, Restaurant Ours is
+        unpretentious, effortless and elegant with a nod to retro chic. An
+        internationally inspired sharing menu that focuses on seasonal, fresh
+        ingredients and flavour. Accompanied by eclectic cocktails and a range
+        of carefully selected wines.
+      </p>
+    </section>
+  );
+};
+
+export default About;
